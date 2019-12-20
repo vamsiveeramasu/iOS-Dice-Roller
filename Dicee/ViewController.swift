454aa929476b7467e5b75ab3ff01cb2a4baad4d9
vamsiveeramasu <vamsiveeramasu@gmail.com>
2019-12-20 09:08:49 -0800

    Full Dicee Application.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let myName = "Vamsi"
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceimageView1: UIImageView! //Interface builder outlet
+    @IBOutlet weak var diceimageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateDiceImages() //To randomize dice faces upon loading up of the app
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceimageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceimageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
 }
 
